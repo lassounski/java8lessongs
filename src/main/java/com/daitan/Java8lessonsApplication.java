40aeccddbf4fd3bf50fd6571a0caccaf05fc8bee
Kirill Lassounski <klassounski@daitangroup.com>
2017-04-16 12:35:10 -0300

    first commit

---
+++ src/main/java/com/daitan/Java8lessonsApplication.java
@@ -0,0 +1,12 @@
+package com.daitan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8lessonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8lessonsApplication.class, args);
+	}
+}
